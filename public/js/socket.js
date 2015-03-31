8135cbad2a83daee7207ef5eb3c09a95578f4d8d
Mishal Islam <mishal.islam01@gmail.com>
2015-03-31 18:25:39 +0100

    displays online users and continously updates

--- public/js/socket.js
+++ public/js/socket.js
@@ -7,20 +7,43 @@ $('#userName').keypress(function(key){
     username = $('#userName').val()
     $('#enterName').hide();
     $('#userText').show();
+    $('#online').show();
+    $('#userTyping').show();
+    socket.emit('add user', username);
   }
 });
 
 $('#m').keypress(function(){
   socket.emit('user is typing', username + ' is typing...')
-})
+});
 
 $('#userText').submit(function(){
   var message = {name: username, message: $('#m').val()}
   socket.emit('chat message', message);
   appendMessage(message)
   $('#m').val('');
-  socket.emit('user is typing', '')
+  socket.emit('user is typing', '');
   return false;
+});
+
+$('#online').click(function(){
+  $('#onlineUsers').show();
+  setInterval(function(){
+    $.ajax({
+      url: '/onlineusers',
+      type: 'POST',
+      success: function(onlineUsers){
+        $('#users').text('')
+        onlineUsers.forEach(function(name){
+          $('#users').append($('<li>').text(name));
+        });
+      }
+    });
+  }, 1000)
+});
+
+$('#exit').click(function(){
+  $('#onlineUsers').hide();
 });
 
 socket.on('send chat message', function(msg){
@@ -28,9 +51,9 @@ socket.on('send chat message', function(msg){
 });
 
 socket.on('display user is typing', function(msg){
-  $('#userTyping').text(msg)
-})
+  $('#userTyping').text(msg);
+});
 
 var appendMessage = function(item){
   $('#messages').append($('<li>').text(item.name + ': ' +item.message));
-}
+};
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http)
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
@@ -12,17 +13,38 @@ app.get('/', function(request, response){
   response.render('index');
 });
 
+app.post('/onlineusers', function(request, response){
+  response.send(onlineUsers)
+});
+
+var onlineUsers = []
+
 io.on('connection', function(socket){
+var currentUser;
+
   console.log('user connected')
+
   socket.on('disconnect', function(){
     console.log('user disconnected')
+    var index = onlineUsers.indexOf(currentUser)
+    onlineUsers.splice(index, 1)
+    console.log(onlineUsers)
   });
+
   socket.on('chat message', function(msg){
     socket.broadcast.emit('send chat message', msg);
   });
+
   socket.on('user is typing', function(msg){
     socket.broadcast.emit('display user is typing', msg)
+  });
+
+  socket.on('add user', function(name){
+    currentUser = name;
+    onlineUsers.push(name)
+    console.log(onlineUsers)
   })
+
 });
 
 http.listen(port, function(){
